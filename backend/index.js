7891f492751bb6efeabd4500b23829ca6ff6a371
sohailaahmed12 <sohaila.hanafy07@eng-st.cu.edu.eg>
2026-07-26 10:41:31 +0300

    backend index file

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: '*' },
+});
+
+io.on('connection', (socket) => {
+  console.log('A client connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
